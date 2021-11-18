976e99c9db637743e13f56858edfaa44281350bd
Nas La Menace <nassermassadimi@gmail.com>
2021-11-18 12:45:53 +0000

    start back-office

---
+++ src/components/AdminMainPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import PageTitle from "./parts/PageTitle";
+
+
+
+const AdminMainPage = ({categories}) => {
+    return (  
+        <div>
+            <PageTitle pageTitle="ESPACE ADMIN" />
+            {categories.map(categorie => (
+                <Link to={`/admin/${categorie}`}>
+                    <h2>{categorie}</h2>
+                </Link>
+            ))}
+        </div>
+    );
+}
+ 
+export default AdminMainPage;
